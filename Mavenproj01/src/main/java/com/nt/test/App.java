42caa4e77d743c549bf2d4f8f2e91c5b475f79e5
PRASANTH <PRASANTH@DESKTOP-91DB8PR.lan>
2025-08-17 11:16:54 +0530

    new maven proj

---
+++ Mavenproj01/src/main/java/com/nt/test/App.java
@@ -0,0 +1,7 @@
+package com.nt.test;
+
+public class App {
+    public double add(double d1,double d2) {
+    	return d1+d2;
+    }
+}
---
+++ Mavenproj01/src/test/java/com/nt/test/AppTest.java
@@ -0,0 +1,50 @@
+package com.nt.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+  
+    @Test
+    public void bignum() {
+        double num1=7000000.0;
+        double num2=8000000.0;
+        App ob=new App();
+        double expected=ob.add(num1, num2);
+        double actual=15000000.0;
+        assertEquals(expected,actual);
+    }
+    
+    @Test
+    public void smallnum() {
+        double num1=700000.0;
+        double num2=800000.0;
+        App ob=new App();
+        double expected=ob.add(num1, num2);
+        double actual=1500000.0;
+        assertEquals(expected,actual);
+    }
+    
+    @Test
+    public void zeros() {
+        double num1=0.0;
+        double num2=-0.0;
+        App ob=new App();
+        double expected=ob.add(num1, num2);
+        double actual=0.0;
+        assertEquals(expected,actual);
+    }
+    
+    
+    @Test
+    public void negatives() {
+        double num1=-7000000.0;
+        double num2=8000000.0;
+        App ob=new App();
+        double expected=ob.add(num1, num2);
+        double actual=1000000.0;
+        assertEquals(expected,actual);
+    }
+}
